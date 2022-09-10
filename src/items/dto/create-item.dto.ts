e1c82f7ad0f4104bf49b539cec972e58b09e19f0
majiyd-fella <majiyd.suleiman@joinfella.com>
2022-09-10 11:08:11 +0100

    create items

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,1 +1,13 @@
-export class CreateItemDto {}
+import { IsBoolean, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateItemDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isForRent: boolean;
+
+  @IsInt()
+  userId: number;
+}
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,11 +1,65 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 
 @Injectable()
 export class ItemsService {
-  create(createItemDto: CreateItemDto) {
-    return 'This action adds a new item';
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createItemData: CreateItemDto): Promise<Item> {
+    const { name, isForRent, userId } = createItemData;
+    const user = await this.prismaService.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        items: {
+          create: {
+            name,
+            isForRent,
+          },
+        },
+      },
+      include: {
+        items: {
+          take: -1,
+        },
+      },
+    });
+
+    return user.items[0];
+  }
+
+  async create2(createItemData: CreateItemDto): Promise<Item> {
+    const { name, isForRent, userId } = createItemData;
+    const item = await this.prismaService.item.create({
+      data: {
+        name,
+        isForRent,
+        User: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+
+    return item;
+  }
+
+  async create3(createItemData: CreateItemDto): Promise<Item> {
+    const { name, isForRent, userId } = createItemData;
+    const item = await this.prismaService.item.create({
+      data: {
+        name,
+        isForRent,
+        userId,
+      },
+    });
+
+    return item;
   }
 
   findAll() {
