fa6b64badfa3203636b167654ba7639d2201fac7
majiyd-fella <majiyd.suleiman@joinfella.com>
2022-09-10 04:00:57 +0100

    generate item resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma/prisma.service';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ItemsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
